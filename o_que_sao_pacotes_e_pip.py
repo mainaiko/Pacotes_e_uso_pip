b9b8372bededae6bf6d8483e3fa18538e372181e
Aiko <mutantetrakor82@gmail.com>
2025-01-09 11:47:24 -0300

    aula 1

---
+++ o_que_sao_pacotes_e_pip.py
@@ -0,0 +1,42 @@
+"""
+Pacotes sao modulos que podem ser instalados e utilizados em programas python. eles permitem
+que voce utilize codigo que foi escrito por outras pessoas, economizando tempo e esforço
+
+O papel do pip 
+Pip é o gerenciador de pacotes do python. ele nos permite instalar, atualizar e remover pacotes facilmente
+Ele se comunica com o PyPI(python package index), que é onde a maioria dos pacotes python sao armazenados
+pypi é um site onde voce encontra os pacotes
+:)
+
+exemplo de codigo:
+
+pip install numpy
+pip uninstall numpy
+pip list
+"""
+
+"""
+Uso de ambientes virtuais
+
+ambientes virtuais, como os criados por venvs, nos permitem manter as dependencias de diferentes projetos.
+isso é importante para evitar conflitos entre versoes de pacotes
+
+exemplo de codigo:
+
+python3 -m venv myenv (criar)
+source myenv/bin/activate (ativar)
+"""
+
+"""
+Comandos pip
+como um programador que esta aprendendo python e deseja gerenciar os pacotes do seu projeto, é importante
+conhecer alguns principais comandos pip
+
+pip install numpy
+pip uninstall numpy
+pip list
+"
+pip install --upgrade nome_do_pacote
+# atualizar um pacote
+"""
+
